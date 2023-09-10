7a6a531956a630b1d43bddac9be2442d0d1f5c8f
al946x <alina.zenam@hotmail.com>
2023-09-10 18:09:32 +0100

    changed var

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -4,55 +4,56 @@ import ai_image from '../assets/ai_image.jpg'
 
 export const ImageGenerator = () => {
 
-  const [image_url,setImage_url] = useState("/");
-  let inputRef = useRef(null);
-  const [loading,setLoading] = useState(false);
+  const [image_url, setImage_url] = useState("/");
+  const [loading, setLoading] = useState(false);
+  var inputRef = useRef(null);
 
-  const ImageGenerator = async () =>{
-    if (inputRef.current.value==="") {
+  const ImageGenerator = async () => {
+    if (inputRef.current.value==="") 
+    {
       return 0;
     }
     setLoading(true);
     const response = await fetch(
       "https://api.openai.com/v1/images/generations",
       {
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
           Authorization:
-          "Bearer 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",
+            "Bearer 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",
           "User-Agent": "Chrome",
-          },
-          body:JSON.stringify({
-            prompt:`${inputRef.current.values}`,
-            n:1,
-            size:"512x512",
-          }),
-        }
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.values}`,
+          n: 1,
+          size: "512x512",
+        }),
+      }
     );
-    let data = await response.json();
-    let data_array = data.data;
-    setImage_url(data_array[0].url);
-    setLoading(false);
-     
+
+    var data = await response.json();
+   var data_array = data.data;
+  setImage_url(data_array[0].url);
+
   }
 
   return (
     <div className='ai-image-generator'>
       <div className="header">Ai Image <span>Generator</span></div>
       <div className='img-loading'>
-        <div className='image'><img src={image_url==="/"?ai_image:image_url} alt="" /></div>
-      <div className="loading"> 
-      <div className={loading?"loading-bar-full": "loading-bar"}></div></div>
-    <div className={loading?"loading-text" : "display-none"}>Loading...</div>
+        <div className='image'><img src={image_url === "/" ? ai_image : image_url} alt="" /></div>
+        <div className="loading">
+          <div className={loading ? "loading-bar-full" : "loading-bar"}></div></div>
+        <div className={loading ? "loading-text" : "display-none"}>Loading...</div>
       </div>
       <div className='search-box'>
         <input type="text" ref={inputRef} className='search-input' placeholder='Desribe What Image You Want Created' />
-      <div className='generate-btn' onClick={()=>{ImageGenerator()}}>Generate</div>
+        <div className='generate-btn' onClick={() => { ImageGenerator() }}>Generate</div>
       </div>
-      </div>
+    </div>
   )
 }
- 
+
 export default ImageGenerator
 
