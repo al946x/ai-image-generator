b0a1074a9a59b66742b484c74b44f3bdc1d3a761
al946x <alina.zenam@hotmail.com>
2023-09-10 17:56:05 +0100

    set loading true set loading false

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -9,9 +9,10 @@ export const ImageGenerator = () => {
   const [loading,setLoading] = useState(false);
 
   const ImageGenerator = async () =>{
-    if (inputRef.current.value=="") {
+    if (inputRef.current.value==="") {
       return 0;
     }
+    setLoading(true);
     const response = await fetch(
       "https://api.openai.com/v1/images/generations",
       {
@@ -32,6 +33,7 @@ export const ImageGenerator = () => {
     let data = await response.json();
     let data_array = data.data;
     setImage_url(data_array[0].url);
+    setLoading(false);
      
   }
 
